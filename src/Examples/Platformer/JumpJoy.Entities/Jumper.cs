3beb802ff71369288825012c4c5fcbb7445c7c97
Jared Barboza <jared.m.barboza@gmail.com>
2013-06-28 21:31:06 -0400

    adds jumpjoy example
    hmmmmm the character looks pretty familiar

---
+++ src/Examples/Platformer/JumpJoy.Entities/Jumper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FragEngine;
+using FragEngine.Animation;
+using FragEngine.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace JumpJoy.Entities
+{
+    public class Jumper : Player
+    {
+        public Jumper()
+        {
+            Collision = CollisionType.A;
+
+            MaxVelocity = new Vector2 { X = 250, Y = 250 };
+
+            Acceleration = 5;
+
+            Animations = new AnimationSheet( @"Textures\player", 64, 64 );
+
+            Animations.Add( "idle", 1f, true, 0, 1 );
+
+            Animations.Add( "run", 0.07f, true, 12, 13, 14, 15, 16, 17, 18, 19, 20 );
+        }
+
+        public override void Update( GameTime time )
+        {
+            float velocity_x = 0, velocity_y = 0;
+
+            Animations.SetCurrentAnimation( "idle" );
+            var keyState = Keyboard.GetState();
+
+            if( keyState.IsKeyDown( Keys.Left ) )
+            {
+                Animations.SetCurrentAnimation( "run" );
+                Animations.CurrentAnimation.FlipX = true;
+                velocity_x = -Acceleration;
+            }
+
+            if( keyState.IsKeyDown( Keys.Right ) )
+            {
+                Animations.SetCurrentAnimation( "run" );
+                velocity_x = Acceleration;
+            }
+
+            Velocity = new Vector2( velocity_x, velocity_y );
+
+            base.Update( time );
+        }
+    }
+}
---
+++ src/Examples/Platformer/JumpJoy/Screens/JumpPlayScreen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FragEngine.Data;
+using FragEngine.Entities;
+using FragEngine.View.Screens;
+using JumpJoy.Entities;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace JumpJoy.Screens
+{
+    public class JumpPlayScreen : PlayScreen
+    {
+        public JumpPlayScreen(HudBase hud) : base(hud)
+        {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            // load the level
+
+            var path = Path.Combine( AppDomain.CurrentDomain.BaseDirectory, "Data\\jumpjoy_1.json" );
+
+            var level = Level.Load( new FileInfo( path ) );
+
+            var jump = level.Entities.First( e => e as Jumper != null );
+
+            ScreenManager.Camera.Target = jump;
+
+            _playerLayer.Entities.Add( jump );
+
+            _layers.InsertRange( 0, level.MapLayers );
+
+            // tell XNA not to apply any texture filtering to our sprites
+            _layers.ForEach( l => l.SamplerState = SamplerState.PointWrap );
+        }
+    }
+}
