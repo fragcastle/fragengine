a36411fe716e9a550dce793bb3386c8a0f60b93b
Jared Barboza <jared.m.barboza@gmail.com>
2016-07-07 12:30:05 -0500

    HAHAHAH FUCK YOU STOCK VS GITIGNORE

---
+++ src/FragEngine/Debug/DebugDecoration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FragEngine.Services;
+using FragEngine.View.Screens;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FragEngine.Debug
+{
+    public class DebugDecoration : Decoration
+    {
+        private readonly FpsCounter _frameCounter = new FpsCounter();
+        private readonly IGameObjectService _gameObjectService;
+        private SpriteBatch _spriteBatch;
+        private Font _font;
+
+        public DebugDecoration(IGameObjectService gameObjectService = null)
+        {
+            _gameObjectService = gameObjectService ?? ServiceLocator.Get<IGameObjectService>();
+            var graphicsDevice = ServiceLocator.Get<GraphicsDevice>();
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(graphicsDevice);
+            _font = ContentCacheManager.GetFont(@"FragEngine.Resources.font_04b03_white_16.png");
+            Name = "FramePerSecond";
+            ZIndex = 100;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            _frameCounter.Update(deltaTime);
+
+            var numberOfObjects = _gameObjectService.GameObjects.Count;
+
+            var fps = string.Format("FPS: {0}\nObjects: {1}", _frameCounter.AverageFramesPerSecond, numberOfObjects);
+
+            _font.Draw(_spriteBatch, fps, Vector2.One);
+        }
+    }
+}
---
+++ src/FragEngine/Debug/FPSCounter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FragEngine.Debug
+{
+    public class FpsCounter
+    {
+        public long TotalFrames { get; private set; }
+        public float TotalSeconds { get; private set; }
+        public float AverageFramesPerSecond { get; private set; }
+        public float CurrentFramesPerSecond { get; private set; }
+
+        public const int MaximumSamples = 100;
+
+        private readonly Queue<float> _sampleBuffer = new Queue<float>();
+
+        public void Update(float deltaTime)
+        {
+            CurrentFramesPerSecond = 1.0f / deltaTime;
+
+            _sampleBuffer.Enqueue(CurrentFramesPerSecond);
+
+            if (_sampleBuffer.Count > MaximumSamples)
+            {
+                _sampleBuffer.Dequeue();
+                AverageFramesPerSecond = _sampleBuffer.Average(i => i);
+            }
+            else
+            {
+                AverageFramesPerSecond = CurrentFramesPerSecond;
+            }
+
+            TotalFrames++;
+            TotalSeconds += deltaTime;
+        }
+    }
+}
